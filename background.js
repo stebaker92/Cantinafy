2932e7a0cc514668bf4159ef8fa1dad830f1429e
Steve Baker <ste.baker.92@gmail.com>
2018-05-31 20:18:08 +0100

    Spaces

--- background.js
+++ background.js
@@ -1,19 +1,19 @@
-$(document).ready(function() {
+$(document).ready(function () {
 	var chance = Math.floor(Math.random() * 100);
-	if(chance == 69) {
+	if (chance == 69) {
 		cantinafy();
 	}
 });
 
-var generateHTML = function() {
+var generateHTML = function () {
 	var html = `<audio controls autoplay>
 	<source src="https://raw.githubusercontent.com/stebaker92/Cantinafy/master/cantinafy.mp3" type="audio/mpeg">
   </audio>`
 	return html;
 };
 
-var cantinafy = function() {
-	setTimeout(function() { // Wait 3 seconds
+var cantinafy = function () {
+	setTimeout(function () { // Wait 3 seconds
 		var html = generateHTML();
 		$('body').append(html);
 	}, 3000);
